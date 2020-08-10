fbfcc32a06457fc23ebe63c40e82a54b9a6d7d41
Daniel Fain <d6niel@protonmail.com>
2020-08-10 17:26:15 -0400

    Add circular progress bar class

---
+++ eyecare/CircularProgressBar.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace eyecare
+{
+    public partial class CircularProgressBar : ProgressBar
+    {
+        public CircularProgressBar()
+        {
+            ValueChanged += CircularProgressBar_ValueChanged;
+        }
+
+        void CircularProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            CircularProgressBar bar = sender as CircularProgressBar;
+            double currentAngle = bar.Angle;
+            double targetAngle = e.NewValue / bar.Maximum * 359.999;
+
+            DoubleAnimation anim = new DoubleAnimation(currentAngle, targetAngle, TimeSpan.FromMilliseconds(500));
+            bar.BeginAnimation(CircularProgressBar.AngleProperty, anim, HandoffBehavior.SnapshotAndReplace);
+        }
+
+        public double Angle
+        {
+            get { return (double)GetValue(AngleProperty); }
+            set { SetValue(AngleProperty, value); }
+        }
+
+        public static readonly DependencyProperty AngleProperty =
+            DependencyProperty.Register("Angle", typeof(double), typeof(CircularProgressBar), new PropertyMetadata(0.0));
+
+        public double StrokeThickness
+        {
+            get { return (double)GetValue(StrokeThicknessProperty); }
+            set { SetValue(StrokeThicknessProperty, value); }
+        }
+
+        public static readonly DependencyProperty StrokeThicknessProperty =
+            DependencyProperty.Register("StrokeThickness", typeof(double), typeof(CircularProgressBar), new PropertyMetadata(10.0));
+    }
+}
